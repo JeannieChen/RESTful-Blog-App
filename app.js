82607645606fe1d47c776abe1a4e62d95e178530
root <jeannie_chen@hotmail.com>
2020-06-03 02:09:33 +0000

    fixted port

--- app.js
+++ app.js
@@ -109,6 +109,12 @@ app.delete("/blogs/:id", function(req, res){
 
 
 // Listening
-app.listen(3000, function(){
-	console.log("Server is running.");
+// app.listen(3000, function(){
+// 	console.log("Server is running.");
+// });
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+app.listen(port, host, function() {
+  console.log("Server started.......");
 });
